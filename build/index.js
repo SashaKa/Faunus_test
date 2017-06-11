6aaf03931fc4e1f0824f89872d4f5313f44f4be7
SashaKa <skrableskru@gmail.com>
2017-06-11 07:44:47 +0300

    with modal-content

---
+++ build/index.js
@@ -0,0 +1,31 @@
+
+const popup = document.querySelector(".modal-content")
+const close = document.querySelector(".modal__close-button")
+const closeOverlay = document.querySelector(".modal-overlay")
+const overlay = document.querySelector(".modal-overlay")
+
+
+
+
+document.addEventListener("mouseleave", ( ) => {
+        popup.classList.add("show-modal-content");
+        popup.classList.add("show-animation");
+        overlay.classList.add("modal-overlay-show");
+    })
+
+close.addEventListener('click', ( ) => {
+        event.preventDefault();
+        popup.classList.remove("show-modal-content");
+        closeOverlay.classList.remove("modal-overlay-show");
+    })
+
+overlay.addEventListener("click", ( ) => {
+        event.preventDefault();
+        overlay.classList.remove("show-modal-content");
+    })
+    
+closeOverlay.addEventListener("click", ( ) => {
+        event.preventDefault();
+        closeOverlay.classList.remove("modal-overlay-show");
+        popup.classList.remove("show-modal-content");
+    })
