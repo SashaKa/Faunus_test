f112085c6f72e959f152cc6c5c919815051eca7a
SashaKa <skrableskru@gmail.com>
2017-06-12 17:05:32 +0300

    readme

--- build/index.js
+++ build/index.js
@@ -8,24 +8,25 @@ const overlay = document.querySelector(".modal-overlay")
 
 
 document.addEventListener("mouseleave", ( ) => {
+        console.log('mouse leave outside');
         popup.classList.add("show-modal-content");
         popup.classList.add("show-animation");
         overlay.classList.add("modal-overlay-show");
     })
 
 close.addEventListener('click', ( ) => {
-        event.preventDefault();
+        // event.preventDefault();
         popup.classList.remove("show-modal-content");
         closeOverlay.classList.remove("modal-overlay-show");
     })
 
 overlay.addEventListener("click", ( ) => {
-        event.preventDefault();
+        // event.preventDefault();
         overlay.classList.remove("show-modal-content");
     })
     
 closeOverlay.addEventListener("click", ( ) => {
-        event.preventDefault();
+        // event.preventDefault();
         closeOverlay.classList.remove("modal-overlay-show");
         popup.classList.remove("show-modal-content");
     })
